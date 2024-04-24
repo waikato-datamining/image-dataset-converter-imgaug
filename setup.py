c554cb9707f3e4f367ed6c07eef5e17d468087a6
Peter Reutemann <fracpete@gmail.com>
2024-04-24 16:44:59 +1200

    fixed packages of tools

--- setup.py
+++ setup.py
@@ -41,8 +41,8 @@ setup(
     author_email='fracpete@waikato.ac.nz',
     entry_points={
         "console_scripts": [
-            "idc-generate-regions=idc.tool.generate_regions:sys_main",
-            "idc-combine-sub-images=idc.tool.combine_sub_images:sys_main",
+            "idc-generate-regions=idc.imgaug.tool.generate_regions:sys_main",
+            "idc-combine-sub-images=idc.imgaug.tool.combine_sub_images:sys_main",
         ],
         "idc.filters": [
             "idc_imgaug_filters1=idc.imgaug.filter:seppl.io.Filter",
